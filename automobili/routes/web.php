f84210fc8b364a719ec04faf384c49ede2b6908d
francesconardella97 <fnardella03@gmail.com>
2023-06-14 21:51:44 +0200

    1-commit correzzioni

--- automobili/routes/web.php
+++ automobili/routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', [app\Http\Controllers\PublicController::class, 'homepage'])->name('homepage');
+Route::get('/', [App\Http\Controllers\PublicController::class, 'homepage'])->name('homepage');
